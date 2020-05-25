578f30463ef4d9816fc38279f50416fa44045a22
orhewett <orhewett@gmail.com>
2020-05-25 21:27:55 +0930

    Initial commit

---
+++ LoadDesktop.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import os
+
+testString = "echo hello there"
+
+root = tk.Tk()
+windowWidth = root.winfo_screenwidth()
+windowHeight = root.winfo_screenheight()
+display = tk.Canvas(root, width=windowWidth, height=windowHeight, bg="#000088")
+display.pack()
+
+with open("desktop/output.txt", "w"):
+
+
+root.mainloop()
